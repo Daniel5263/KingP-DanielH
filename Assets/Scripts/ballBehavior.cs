87c1a528104bc34d207379b02e47896572a7dfbb
Daniel5263 <143121517+Daniel5263@users.noreply.github.com>
2025-01-22 15:50:25 -0600

    Ball speed controls
    Added:
    - Ball speed controls for each prefab

--- Assets/Scripts/ballBehavior.cs
+++ Assets/Scripts/ballBehavior.cs
@@ -3,23 +3,33 @@ using UnityEngine;
 
 public class ballBehavior : MonoBehaviour
 {
-    public float minX = -8.01f;
-    public float minY = -4.16f;
-    public float maxX = 7.8f;
-    public float maxY = 4.34f;
+    public float minX = -10.102f;
+    public float minY = -4.219f;
+    public float maxX = 10.102f;
+    public float maxY = 4.219f;
     public float minSpeed;
     public float maxSpeed;
     public Vector2 targetPosition;
 
     public int secondToMaxSpeed;
 
+    public GameObject target;
+    public float minLaunchSpeed;
+    public float maxLaunchSpeed;
+    public float minTimeToLaunch;
+    public float maxTimeToLaunch;
+    public float cooldown;
+    public bool launching;
+    public float launchDuration;
+    public float timeLastLaunch;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        secondToMaxSpeed = 30;
-        minSpeed = 0.75f;
-        maxSpeed = 2.0f;
+       // secondToMaxSpeed = 30;
+       // minSpeed = 0.001f;
+       // maxSpeed = 2.0f;
         targetPosition = getRandomPosition();
     }
 
@@ -27,9 +37,12 @@ public class ballBehavior : MonoBehaviour
     void Update()
     {
         Vector2 currentPos = gameObject.GetComponent<Transform>().position;
+        float distance = Vector2.Distance(currentPos, targetPosition);
 
-        if(targetPosition != currentPos) {
-            float currentSpeed = minSpeed;
+        if(distance > 0.1) {
+            float difficulty = getDifficultypercentage();
+            float currentSpeed = Mathf.Lerp(minSpeed, maxSpeed, difficulty);
+            currentSpeed = currentSpeed * Time.deltaTime;
             Vector2 newPosition = Vector2.MoveTowards(currentPos, targetPosition, currentSpeed);
             transform.position = newPosition;
         } else {
@@ -45,5 +58,16 @@ public class ballBehavior : MonoBehaviour
 
         Vector2 v = new Vector2(randX, randY);
         return v;
+    }
+
+    public float getDifficultypercentage()
+    {
+        float difficulty = Mathf.Clamp01(Time.timeSinceLevelLoad / secondToMaxSpeed);
+        return difficulty;
+    }
+
+    public void launch()
+    {
+
     }
 }
