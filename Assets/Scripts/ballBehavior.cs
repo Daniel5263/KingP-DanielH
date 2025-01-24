e76ecf27fe6ff1b4451bdd95ac206498608db3b7
Daniel5263 <143121517+Daniel5263@users.noreply.github.com>
2025-01-24 15:50:55 -0600

    Updated Ball Behavior
    Added:
    - Follow the pin

--- Assets/Scripts/ballBehavior.cs
+++ Assets/Scripts/ballBehavior.cs
@@ -1,4 +1,5 @@
 using Unity.VisualScripting;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class ballBehavior : MonoBehaviour
@@ -18,6 +19,7 @@ public class ballBehavior : MonoBehaviour
     public float maxLaunchSpeed;
     public float minTimeToLaunch;
     public float maxTimeToLaunch;
+    public float timeLaunchStart;
     public float cooldown;
     public bool launching;
     public float launchDuration;
@@ -36,21 +38,43 @@ public class ballBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(launching == false && onCooldown() == false)
+        {
+            launch();
+        }
+
         Vector2 currentPos = gameObject.GetComponent<Transform>().position;
         float distance = Vector2.Distance(currentPos, targetPosition);
 
         if(distance > 0.1) {
             float difficulty = getDifficultypercentage();
-            float currentSpeed = Mathf.Lerp(minSpeed, maxSpeed, difficulty);
+            float currentSpeed;
+
+            if (launching == true)
+            {
+                float launchingForHowLong = Time.time - timeLaunchStart;
+                if(launchingForHowLong > launchDuration)
+                {
+                    startCooldown();
+                }
+                currentSpeed = Mathf.Lerp(minLaunchSpeed, maxLaunchSpeed, difficulty);
+
+            } else
+            {
+                currentSpeed = Mathf.Lerp(minSpeed, maxSpeed, difficulty);
+            }
             currentSpeed = currentSpeed * Time.deltaTime;
             Vector2 newPosition = Vector2.MoveTowards(currentPos, targetPosition, currentSpeed);
             transform.position = newPosition;
         } else {
+            if (launching == true)
+            {
+                startCooldown();
+            }
             targetPosition = getRandomPosition();
         }
-
-        getRandomPosition();
     }
+
     Vector2 getRandomPosition()
     {
         float randX = Random.Range(minX, maxX);
@@ -68,6 +92,31 @@ public class ballBehavior : MonoBehaviour
 
     public void launch()
     {
+        targetPosition = target.transform.position;
 
+        if (launching == false)
+        {
+            timeLaunchStart = Time.time;
+            launching = true;
+        }
+    }
+
+    public bool onCooldown()
+    {
+        bool result = false;
+
+        float timeSinceLastLaunch = Time.time - timeLastLaunch;
+
+        if (timeSinceLastLaunch < cooldown)
+        {
+            result = true;
+        }
+        return result;
+    }
+
+    public void startCooldown()
+    {
+        timeLastLaunch = Time.time;
+        launching = false;
     }
 }
