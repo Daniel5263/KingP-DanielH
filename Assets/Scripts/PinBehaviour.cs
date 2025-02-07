b0a299c3f600190df6b77a54a61c3b28ad611796
Daniel5263 <143121517+Daniel5263@users.noreply.github.com>
2025-02-07 12:39:24 -0600

    Added walls

--- Assets/Scripts/PinBehaviour.cs
+++ Assets/Scripts/PinBehaviour.cs
@@ -1,10 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public class PinBehaviour : MonoBehaviour{
-    public float speed = 2.0f;
+    public float speed;
     public Vector2 newPosition;
     public Vector3 mousePosG;
     Camera cam;
+
+    Rigidbody2D body;
 
     void Start(){
         cam = Camera.main;
@@ -12,8 +15,28 @@ public class PinBehaviour : MonoBehaviour{
     }
 
     void Update(){
+        
+    }
+
+    private void FixedUpdate()
+    {
         mousePosG = cam.ScreenToWorldPoint(Input.mousePosition);
         newPosition = Vector2.MoveTowards(transform.position, mousePosG, speed * Time.fixedDeltaTime);
         transform.position = newPosition;
+
+        body = GetComponent<Rigidbody2D>();
+        Vector2 currentPostion = body.position;
+
+        body.MovePosition(newPosition);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        string collided = collision.gameObject.tag;
+        Debug.Log("Collided with" + collided);
+        if (collided == "Ball" || collided == "Wall")
+        {
+            Debug.Log("Game Over");
+        }
     }
 }
--- Assets/Scripts/ballBehavior.cs
+++ Assets/Scripts/ballBehavior.cs
@@ -4,119 +4,161 @@ using UnityEngine;
 
 public class ballBehavior : MonoBehaviour
 {
-    public float minX = -10.102f;
-    public float minY = -4.219f;
-    public float maxX = 10.102f;
-    public float maxY = 4.219f;
+    public float minX = -4.84f;
+    public float minY = -3.76f;
+    public float maxX = 9.6f;
+    public float maxY = 3.83f;
     public float minSpeed;
     public float maxSpeed;
     public Vector2 targetPosition;
 
-    public int secondToMaxSpeed;
-
     public GameObject target;
+    public bool launching;
+    public float timeLastLaunch;
+    public float launchDuration;
+    public float timeLaunchStart;
     public float minLaunchSpeed;
     public float maxLaunchSpeed;
-    public float minTimeToLaunch;
-    public float maxTimeToLaunch;
-    public float timeLaunchStart;
     public float cooldown;
-    public bool launching;
-    public float launchDuration;
-    public float timeLastLaunch;
 
+    public int secondsToMaxSpeed;
+
+    Rigidbody2D body;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       // secondToMaxSpeed = 30;
-       // minSpeed = 0.001f;
-       // maxSpeed = 2.0f;
+        //secondsToMaxSpeed = 30;
+        //minSpeed = 0.001f;
+        //maxSpeed = 2.0f;
         targetPosition = getRandomPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(launching == false && onCooldown() == false)
+        
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 currentPos = gameObject.GetComponent<Transform>().position;
+
+        body = GetComponent<Rigidbody2D>();
+        Vector2 currentPostion = body.position;
+
+        if (onCooldown() == false)
         {
-            launch();
+            if (launching == true)
+            {
+                float currentLaunchTime = Time.time - timeLaunchStart;
+                if (currentLaunchTime > launchDuration)
+                {
+                    startCooldown();
+                }
+            }
+            else
+            {
+                Debug.Log("unim");
+                launch();
+            }
         }
 
-        Vector2 currentPos = gameObject.GetComponent<Transform>().position;
         float distance = Vector2.Distance(currentPos, targetPosition);
-
-        if(distance > 0.1) {
-            float difficulty = getDifficultypercentage();
+        if (distance > 0.1f)
+        {
+            float difficulty = getDifficultyPercentage();
             float currentSpeed;
 
             if (launching == true)
             {
                 float launchingForHowLong = Time.time - timeLaunchStart;
-                if(launchingForHowLong > launchDuration)
+                if (launchingForHowLong > launchDuration)
                 {
                     startCooldown();
                 }
                 currentSpeed = Mathf.Lerp(minLaunchSpeed, maxLaunchSpeed, difficulty);
-
-            } else
+            }
+            else
             {
                 currentSpeed = Mathf.Lerp(minSpeed, maxSpeed, difficulty);
             }
             currentSpeed = currentSpeed * Time.deltaTime;
             Vector2 newPosition = Vector2.MoveTowards(currentPos, targetPosition, currentSpeed);
+            body.MovePosition(newPosition);
             transform.position = newPosition;
-        } else {
+        }
+        else
+        { // You are at target
             if (launching == true)
             {
                 startCooldown();
             }
             targetPosition = getRandomPosition();
+
+            Debug.Log(targetPosition + " on t");
         }
     }
 
     Vector2 getRandomPosition()
     {
-        float randX = Random.Range(minX, maxX);
-        float randY = Random.Range(minY, maxY);
-
-        Vector2 v = new Vector2(randX, randY);
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+        Vector2 v = new Vector2(randomX, randomY);
         return v;
     }
 
-    public float getDifficultypercentage()
+    public float getDifficultyPercentage()
     {
-        float difficulty = Mathf.Clamp01(Time.timeSinceLevelLoad / secondToMaxSpeed);
+        float difficulty = Mathf.Clamp01(Time.timeSinceLevelLoad / secondsToMaxSpeed);
         return difficulty;
+    }
+
+    public void initialPosition()
+    {
+        body = GetComponent<Rigidbody2D>();
+        body.position = getRandomPosition();
     }
 
     public void launch()
     {
+        Rigidbody2D targetBody = target.GetComponent<Rigidbody2D>();
         targetPosition = target.transform.position;
 
         if (launching == false)
         {
             timeLaunchStart = Time.time;
             launching = true;
+            Debug.Log(launching + " on L");
         }
     }
 
     public bool onCooldown()
     {
-        bool result = false;
-
         float timeSinceLastLaunch = Time.time - timeLastLaunch;
-
         if (timeSinceLastLaunch < cooldown)
         {
-            result = true;
+            return true;
         }
-        return result;
+        return false;
     }
+
 
     public void startCooldown()
     {
         timeLastLaunch = Time.time;
         launching = false;
     }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            targetPosition = getRandomPosition();
+        }
+       // Debug.Log(this + "Collided with: " + collision.gameObject.name);
+    }
+
 }
